ffcc361b7de7f9dfe609bbab7000a045ce3a066e
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2019-04-03 13:31:35 -0400

    Add test case for constant quantile

--- waterz/tests/test_quantile_function.py
+++ waterz/tests/test_quantile_function.py
@@ -11,37 +11,18 @@ class TestQuantileFunction(unittest.TestCase):
 
     def test_simple(self):
 
-        # TODO: what if k > n?
-        # quantile_function = '''
-# Add<
-    # Constant<RegionGraphType, {q}>,
-    # Divide<
-        # Multiply<
-            # Constant<RegionGraphType, {k}>,
-            # Subtract<
-                # Constant<RegionGraphType, 100>,
-                # Constant<RegionGraphType, {q}>
-            # >
-        # >,
-        # ContactArea<RegionGraphType>
-    # >
-# >
-# '''.format(q=75, k=10)
-
-        quantile_function = 'Constant<RegionGraphType, 75>'
-
         score_function = '''
 OneMinus<
-    HistogramQuantileFunctionAffinity<
+    HistogramQuantileAffinity<
         RegionGraphType,
-        %s,
+        75,
         float,
         256>
->'''%quantile_function
+>'''
 
         affs = np.ones(shape=(3, 4, 4, 4), dtype=np.float32)
         affs[0] = 0.4
-        affs[0,:,0,0] = 0.6
+        affs[0, :, 0, 0] = 0.6
         thresholds = [0, 0.5]
         fragments = np.ones(shape=(4, 4, 4), dtype=np.uint64)
         fragments[1] = 2
@@ -54,7 +35,7 @@ OneMinus<
                 scoring_function=score_function,
                 return_merge_history=True,
                 return_region_graph=True,
-                force_rebuild=False)
+                force_rebuild=True)
 
         segmentation, _, _ = next(generator)
         np.testing.assert_array_equal(
@@ -64,3 +45,57 @@ OneMinus<
         segmentation, _, _ = next(generator)
         self.assertEqual(len(np.unique(segmentation)), 1)
 
+    def test_simple_function(self):
+
+        # TODO: what if k > n?
+        quantile_function = '''
+Add<
+    Constant<RegionGraphType, {q}>,
+    Divide<
+        Multiply<
+            Constant<RegionGraphType, {k}>,
+            Subtract<
+                Constant<RegionGraphType, 100>,
+                Constant<RegionGraphType, {q}>
+            >
+        >,
+        ContactArea<RegionGraphType>
+    >
+>
+'''.format(q=75, k=10)
+
+        # quantile_function = 'Constant<RegionGraphType, 75>'
+
+        score_function = '''
+OneMinus<
+    HistogramQuantileFunctionAffinity<
+        RegionGraphType,
+        %s,
+        float,
+        256>
+>''' % quantile_function
+
+        affs = np.ones(shape=(3, 4, 4, 4), dtype=np.float32)
+        affs[0] = 0.4
+        affs[0, :, 0, 0] = 0.6
+        thresholds = [0, 0.5]
+        fragments = np.ones(shape=(4, 4, 4), dtype=np.uint64)
+        fragments[1] = 2
+        fragments[2] = 3
+        fragments[3] = 4
+        generator = waterz.agglomerate(
+                affs=affs,
+                thresholds=thresholds,
+                fragments=fragments,
+                scoring_function=score_function,
+                return_merge_history=True,
+                return_region_graph=True,
+                force_rebuild=True)
+
+        segmentation, _, _ = next(generator)
+        np.testing.assert_array_equal(
+            segmentation,
+            fragments)
+
+        segmentation, _, _ = next(generator)
+        self.assertEqual(len(np.unique(segmentation)), 1)
